8a2aaf7ddbf628f11ca53a319b8cb7d0999ccfcf
Sachin Rajput <165654923+sachinrajput9810@users.noreply.github.com>
2024-09-04 15:20:18 +0530

    Added portfolio website

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".multiple-text" , {
+    strings : ["Frontend Developer" , "Backend Developer" ] ,
+    typeSpeed : 100 ,
+    backSpeed : 100 ,
+    loop : true ,
+    showCursor : true ,
+
+})
